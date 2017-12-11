401a9ec3f17d257b7c290fb3f861390d665c9ede
Peter Larsen <petertretorn@gmail.com>
2017-12-11 16:19:20 +0100

    firebase functions added to project

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+
+const cors = require('cors')({ origin: true });
+
+const SENDGRID_API_KEY = functions.config().sendgrid.key
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
